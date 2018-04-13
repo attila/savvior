2eadc167d9057877ad213a32f1b30f88dc4f1b5e
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2018-04-13 16:07:33 +0200

    Fix AMD loader with enquire.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,6 +96,7 @@ module.exports = function(grunt) {
         indent: '  ',
         deps: {
           'default': ['enquire'],
+          amd: ['enquire.js'],
           cjs: ['enquire.js']
         }
       },
